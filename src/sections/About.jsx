b8933771839537bb8e0eb54a8c1fc4ec3941f412
Manan Desai <115714410+MananDesai1783@users.noreply.github.com>
2025-06-20 09:05:41 -0400

    Update About.jsx

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -10,21 +10,20 @@ const About = () => {
     <section className="c-space section-spacing" id="about">
       <h2 className="text-heading">About Me</h2>
       <div className="grid grid-cols-1 gap-4 md:grid-cols-6 md:auto-rows-[18rem] mt-12">
-        {/* Grid 1 */}
-        <div className="flex items-end grid-default-color grid-1">
-          <img
-            src="assets/coding-pov.png"
-            className="absolute scale-[1.75] -right-[5rem] -top-[1rem] md:scale-[3] md:left-50 md:inset-y-10 lg:scale-[2.5]"
-          />
-          <div className="z-10">
-            <p className="headtext">Hi, I'm Manan Desai, </p>
-            <p className="subtext">
-            Data Analyst & Strategic Researcher passionate about turning complex data into actionable insights using Python, SQL, AWS, Power BI and Tableau.
-Currently expanding expertise in artificial intelligence to drive smarter healthcare and finance solutions
-            </p>
-          </div>
-          <div className="absolute inset-x-0 pointer-evets-none -bottom-4 h-1/2 sm:h-1/3 bg-gradient-to-t from-indigo" />
-        </div>
+{/* Grid 5 */}
+<div className="grid-default-color grid-5">
+  <div className="z-10 w-[50%]">
+    <p className="headText">Tech Stack</p>
+    <p className="subtext">
+      I specialize in a variety of languages, frameworks, and tools that
+      allow me to build robust and scalable applications
+    </p>
+  </div>
+  <div className="absolute inset-y-0 md:inset-y-9 w-full h-full start-[50%] md:scale-125">
+    <Frameworks />
+  </div>
+</div>
+        
         {/* Grid 2 */}
         <div className="grid-default-color grid-2">
           <div
@@ -100,21 +99,23 @@ Currently expanding expertise in artificial intelligence to drive smarter health
     </p>
   </div>
 </div>
-
-        {/* Grid 5 */}
-        <div className="grid-default-color grid-5">
-          <div className="z-10 w-[50%]">
-            <p className="headText">Teck Stack</p>
+{/* Grid 1 */}
+        <div className="flex items-end grid-default-color grid-1">
+          <img
+            src="assets/coding-pov.png"
+            className="absolute scale-[1.75] -right-[5rem] -top-[1rem] md:scale-[3] md:left-50 md:inset-y-10 lg:scale-[2.5]"
+          />
+          <div className="z-10">
+            <p className="headtext">Hi, I'm Manan Desai, </p>
             <p className="subtext">
-              I specialize in a variety of languages, frameworks, and tools that
-              allow me to build robust and scalable applications
+            Data Analyst & Strategic Researcher passionate about turning complex data into actionable insights using Python, SQL, AWS, Power BI and Tableau.
+Currently expanding expertise in artificial intelligence to drive smarter healthcare and finance solutions
             </p>
           </div>
-          <div className="absolute inset-y-0 md:inset-y-9 w-full h-full start-[50%] md:scale-125">
-            <Frameworks />
-          </div>
+          <div className="absolute inset-x-0 pointer-evets-none -bottom-4 h-1/2 sm:h-1/3 bg-gradient-to-t from-indigo" />
         </div>
-      </div>
+      
+      
     </section>
   );
 };
