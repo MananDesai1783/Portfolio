a87fe55d9ea54b647b6d11efc46b2b48d3fe5823
Manan Desai <115714410+MananDesai1783@users.noreply.github.com>
2025-06-20 07:51:11 -0400

    Update About.jsx

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -17,9 +17,10 @@ const About = () => {
             className="absolute scale-[1.75] -right-[5rem] -top-[1rem] md:scale-[3] md:left-50 md:inset-y-10 lg:scale-[2.5]"
           />
           <div className="z-10">
-            <p className="headtext">Hi, I'm Manan Desai</p>
+            <p className="headtext">Hi, I'm Manan Desai, </p>
             <p className="subtext">
-              
+            a Data Analyst at CVS Health with expertise in Python, SQL, Power BI and AWS.  
+            I love turning complex data into clear, actionable insights.
             </p>
           </div>
           <div className="absolute inset-x-0 pointer-evets-none -bottom-4 h-1/2 sm:h-1/3 bg-gradient-to-t from-indigo" />
