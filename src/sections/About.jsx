edc9d67442cea24f50f95ea41e807b408f388f2c
Manan Desai <115714410+MananDesai1783@users.noreply.github.com>
2025-06-20 09:37:29 -0400

    Update About.jsx

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -2,7 +2,7 @@ import { useRef } from "react";
 import Card from "../components/Card";
 import { Globe } from "../components/globe";
 import CopyEmailButton from "../components/CopyEmailButton";
-import { Frameworks } from "../components/Frameworks"; 
+import { Frameworks } from "../components/FrameWorks";
 
 const About = () => {
   const grid2Container = useRef();
@@ -10,23 +10,21 @@ const About = () => {
     <section className="c-space section-spacing" id="about">
       <h2 className="text-heading">About Me</h2>
       <div className="grid grid-cols-1 gap-4 md:grid-cols-6 md:auto-rows-[18rem] mt-12">
-
-        {/* Grid 5 (was Grid 1) */}
+        {/* Grid 1 */}
         <div className="flex items-end grid-default-color grid-1">
           <img
             src="assets/coding-pov.png"
             className="absolute scale-[1.75] -right-[5rem] -top-[1rem] md:scale-[3] md:left-50 md:inset-y-10 lg:scale-[2.5]"
           />
           <div className="z-10">
-            <p className="headtext">Hi, I'm Manan Desai, </p>
-            <p className="subtext"> 
-              Data Analyst & Strategic Researcher passionate about turning complex data into actionable insights using Python, SQL, AWS, Power BI and Tableau.
-              Currently expanding expertise in artificial intelligence to drive smarter healthcare and finance solutions
+            <p className="headtext">Hi, I'm Ali Sanati</p>
+            <p className="subtext">
+              Over the last 4 years, I developed my frontend and backend dev
+              skills to deliver dynamic and software and web applications.
             </p>
           </div>
           <div className="absolute inset-x-0 pointer-evets-none -bottom-4 h-1/2 sm:h-1/3 bg-gradient-to-t from-indigo" />
         </div>
-
         {/* Grid 2 */}
         <div className="grid-default-color grid-2">
           <div
